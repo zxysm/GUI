a8861f2074d03ef40f1e8d75e960c9fbde5dc635
Paul Robert Buck <paul_buck86@hotmail.com>
2016-08-08 17:52:57 -0400

    Push nuget pkgs

---
+++ PokemonGo.RocketAPI/Client.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using PokemonGo.RocketAPI.Enums;
+using PokemonGo.RocketAPI.Extensions;
+using PokemonGo.RocketAPI.HttpClient;
+using POGOProtos.Networking.Envelopes;
+
+namespace PokemonGo.RocketAPI
+{
+    public class Client
+    {
+        public Rpc.Login Login;
+        public Rpc.Player Player;
+        public Rpc.Download Download;
+        public Rpc.Inventory Inventory;
+        public Rpc.Map Map;
+        public Rpc.Fort Fort;
+        public Rpc.Encounter Encounter;
+        public Rpc.Misc Misc;
+
+        public IApiFailureStrategy ApiFailure { get; set; }
+        public ISettings Settings { get; }
+        public string AuthToken { get; set; }
+
+        internal static WebProxy Proxy;
+
+        public double CurrentLatitude { get; internal set; }
+        public double CurrentLongitude { get; internal set; }
+        public double CurrentAltitude { get; internal set; }
+
+        public AuthType AuthType => Settings.AuthType;
+
+        internal readonly PokemonHttpClient PokemonHttpClient;
+        internal string ApiUrl { get; set; }
+        internal AuthTicket AuthTicket { get; set; }
+
+        public Client(ISettings settings, IApiFailureStrategy apiFailureStrategy)
+        {
+            Settings = settings;
+            ApiFailure = apiFailureStrategy;
+            InitProxy(settings);
+            PokemonHttpClient = new PokemonHttpClient();
+            Login = new Rpc.Login(this);
+            Player = new Rpc.Player(this);
+            Download = new Rpc.Download(this);
+            Inventory = new Rpc.Inventory(this);
+            Map = new Rpc.Map(this);
+            Fort = new Rpc.Fort(this);
+            Encounter = new Rpc.Encounter(this);
+            Misc = new Rpc.Misc(this);
+
+            Player.SetCoordinates(Settings.DefaultLatitude, Settings.DefaultLongitude, Settings.DefaultAltitude);
+        }
+
+        private void InitProxy(ISettings settings)
+        {
+            int port;
+            if (!settings.UseProxy || string.IsNullOrWhiteSpace(settings.UseProxyHost) || string.IsNullOrWhiteSpace(settings.UseProxyPort) || !int.TryParse(settings.UseProxyPort, out port)) return;
+            var proxyString = settings.UseProxyHost.Contains("http://") ? $"{settings.UseProxyHost}:{settings.UseProxyPort}" : $"http://{settings.UseProxyHost}:{settings.UseProxyPort}";
+            Proxy = new WebProxy(proxyString);
+
+            if (settings.UseProxyAuthentication && !string.IsNullOrWhiteSpace(settings.UseProxyUsername) && !string.IsNullOrWhiteSpace(settings.UseProxyPassword))
+                Proxy.Credentials = new NetworkCredential(settings.UseProxyUsername, settings.UseProxyPassword);
+
+        }
+    }
+}
